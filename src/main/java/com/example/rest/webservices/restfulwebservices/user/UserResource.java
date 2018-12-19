feaf51bfd0e3db903d3a60d6f59663984196de35
Carolina Gomez <carolinagomez@MacBook-Air-de-Carolina.local>
2018-12-18 18:53:25 -0600

    Se creó ejemplo de hello world. Se crea bean User y DAO

---
+++ src/main/java/com/example/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,14 @@
+package com.example.rest.webservices.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	private UserDaoService user;
+	public List<User> retrieveAllUsers(){
+		
+	}
+}
